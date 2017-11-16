66e99e0a4893b2ab57b7e94341d487e39906b1dd
yuhi_deve <yuhinnn@gmail.com>
2017-11-16 18:34:05 +0900

    change content

--- index.php
+++ index.php
@@ -8,11 +8,7 @@
 <body>
 
 <div class="relative">
-  <img class="lp"src="lp.jpg" alt="">
-  <a href="https://ws.formzu.net/fgen/S81114783/"><img class="btn-top"src="btn.png" alt=""></a>
-  <a href="https://ws.formzu.net/fgen/S81114783/"><img class="btn-bottom"src="btn.png" alt=""></a>
-  <a href="https://twitter.com/kawaiimap"><img class="twitter"src="twitter.png" alt=""></a>
-  <a href="mailto:omlts.abekobe@gmail.com"><img class="mail"src="mail.png" alt=""></a>
+  <img class="lp"src="lp2.jpg" alt="">
 </div>
 
 </body>
---
+++ index.php_1116.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>kawaiimap</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="relative">
+  <img class="lp"src="lp.jpg" alt="">
+  <a href="https://ws.formzu.net/fgen/S81114783/"><img class="btn-top"src="btn.png" alt=""></a>
+  <a href="https://ws.formzu.net/fgen/S81114783/"><img class="btn-bottom"src="btn.png" alt=""></a>
+  <a href="https://twitter.com/kawaiimap"><img class="twitter"src="twitter.png" alt=""></a>
+  <a href="mailto:omlts.abekobe@gmail.com"><img class="mail"src="mail.png" alt=""></a>
+</div>
+
+</body>
+</html>
+
