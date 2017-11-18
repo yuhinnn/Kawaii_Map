f9b3ec8fb908db695a2ee2180610948ecde5c1a5
yuhi_deve <yuhinnn@gmail.com>
2017-11-18 15:01:02 +0900

    add fi

--- index.php
+++ index.php
@@ -8,7 +8,24 @@
 <body>
 
 <div class="relative">
-  <img class="lp"src="lp6.jpg" alt="">
+  <img class="lp"src="LP7.jpg" alt="">
+  <div class="btn-top">  <p>アイドルを主軸とした芸能プロダクシ<br>ョンならでは！
+現役アイドルも撮影会に参加します！</p> </div>
+</div>
+
+  <div class="btn-top2">  <p>モデルさんの頑張りは常に評価いた<br>します！最大２０％歩合報酬アップも！？※当社規定あり</p> </div>
+</div>
+
+  <div class="btn-top3">  <p>撮影時はモデル１人に対して１人スタ<br>ッフが付きます！</p> </div>
+</div>
+
+  <div class="btn-top4">  <p>撮影会モデル未経験の方でも全く問<br>題ございません！モデルをやってく中楽しくポージングや表情を身につけていきましょう！</p> </div>
+</div>
+
+  <div class="btn-top5">  <p>※募集フォームより応募後、面接をしていただきます。人数が確定し次第募集を締め切らせていただくこともございます。</p> </div>
+</div>
+
+  <div class="btn-top6">  <p>2017オモロタスProduction All Rights Reserved </p> </div>
 </div>
 
 </body>
