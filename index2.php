e9d3126e8bdecef3cef72a90b46dfb7bb0c49fa1
yuhi_deve <yuhinnn@gmail.com>
2017-11-21 18:46:50 +0900

    asdf

--- index2.php
+++ index2.php
@@ -13,19 +13,19 @@
   <div class="btn-top_q">  <p>SNSに写真はアップされたりしますか？</p> </div>
 </div>
 
-  <div class="btn-top_q2">  <p>SNSに関しましては、モデル本人様の<br>ご要望にお答えします！アップしたい写<br>真NGな写真があればお気軽にお申し付<br>けください！</p> </div>
+  <div class="btn-top_q2">  <p>SNSに関しましては、モデル本人様の<br>ご要望にお答えします!アップしたい写<br>真NGな写真があればお気軽にお申し付<br>けください！弊社HPに実際の写真とし<br>UPさせて頂く場合がございます。勝手<br>にカメラマンがSNSにUPすることはご<br>ざいません。</p> </div>
 </div>
 
   <div class="btn-top_q3">  <p>急に参加したい場合は可能ですか？</p> </div>
 </div>
 
-  <div class="btn-top_q4">  <p>可能です！撮影会開始時刻に間に合う<br>ように来ていただければご参加可能で<br>す！</p> </div>
+  <div class="btn-top_q4">  <p>可能です！撮影会開始時刻に間に合う<br>ように来ていただければご参加可能で<br>す！ですがカメラマンさんを募る場面<br>が必要です（二週間程度）参加は可能<br>ですが、できるだけ早くお申し付けく<br>ださい。</p> </div>
 </div>
 
   <div class="btn-top_q5">  <p>露出度の高い衣装の撮影はありますか？</p> </div>
 </div>
 
-  <div class="btn-top_q6">  <p>ございます。ですが予め水着、下着で<br>の撮影がNGかOKかお申し付けくだ<br>さい。</p> </div>
+  <div class="btn-top_q6">  <p>企画によってはございます。ですが予め<br>水着、下着での撮影がNGかOKかお申し<br>付けください。参加NGでも大丈夫です。</p> </div>
 </div>
 
   <div class="btn-top_q7">  <p>モデルの経験や、撮影会経験がない<br>のですが・・・</p> </div>
