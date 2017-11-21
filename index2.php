f88b92e273cc85f142f1d37142ee3ce714021c29
yuhi_deve <yuhinnn@gmail.com>
2017-11-21 16:19:02 +0900

    add swp

--- index2.php
+++ index2.php
@@ -8,24 +8,30 @@
 <body>
 
 <div class="relative">
-  <img class="lp"src="q.jpg" alt="">
+  <img class="lp"src="questionpage.png" alt="">
 
-  <div class="btn-top">  <p>SNSに写真はアップされたりしますか？</p> </div>
+  <div class="btn-top_q">  <p>SNSに写真はアップされたりしますか？</p> </div>
 </div>
 
-  <div class="btn-top2">  <p>モデルさんの頑張りは常に評価いた<br>します！最大２０％歩合報酬アップも！？※当社規定あり</p> </div>
+  <div class="btn-top_q2">  <p>SNSに関しましては、モデル本人様の<br>ご要望にお答えします！アップしたい写<br>真NGな写真があればお気軽にお申し付<br>けください！</p> </div>
 </div>
 
-  <div class="btn-top3">  <p>撮影時はモデル１人に対して１人スタ<br>ッフが付きます！</p> </div>
+  <div class="btn-top_q3">  <p>急に参加したい場合は可能ですか？</p> </div>
 </div>
 
-  <div class="btn-top4">  <p>撮影会モデル未経験の方でも全く問<br>題ございません！モデルをやってく中楽しくポージングや表情を身につけていきましょう！</p> </div>
+  <div class="btn-top_q4">  <p>可能です！撮影会開始時刻に間に合う<br>ように来ていただければご参加可能で<br>す！</p> </div>
 </div>
 
-  <div class="btn-top5">  <p>※募集フォームより応募後、面接をしていただきます。人数が確定し次第募集を締め切らせていただくこともございます。</p> </div>
+  <div class="btn-top_q5">  <p>露出度の高い衣装の撮影はありますか？</p> </div>
 </div>
 
-  <div class="btn-top6">  <p>2017オモロタスProduction All Rights Reserved </p> </div>
+  <div class="btn-top_q6">  <p>ございます。ですが予め水着、下着で<br>の撮影がNGかOKかお申し付けくだ<br>さい。</p> </div>
+</div>
+
+  <div class="btn-top_q7">  <p>モデルの経験や、撮影会経験がない<br>のですが・・・</p> </div>
+</div>
+
+  <div class="btn-top_q8">  <p>問題ございません！まずは笑顔で参<br>加していただければ大丈夫です！</p> </div>
 </div>
 
 </body>
